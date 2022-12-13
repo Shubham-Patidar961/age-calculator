63410e4071f6976fffe6c04848f4fb4d116ffe1c
shubha <shubhampatidar961@gmial.com>
2022-12-13 17:32:58 +0530

    age calculate

---
+++ app.js
@@ -0,0 +1,12 @@
+let btnclick = document.querySelector(".btn");
+let btn = document.querySelector(".btnclick");
+// code for age clculater //
+btnclick.addEventListener("click", () => {
+  const birthyear = Number(prompt("plese provide birth year "));
+  let birthyearcla = 2022 - birthyear;
+  if (birthyear >= 2023 || birthyear <= 0) {
+    btn.innerHTML = "plese provide valid input ";
+  } else {
+    btn.innerHTML = ` you are ${birthyearcla} year old`;
+  }
+});
